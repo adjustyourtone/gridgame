98b2f695a45a7dcd544fee326f513036afee3ef1
Tony Rizzotto <anthonyrizzotto@gmail.com>
2020-10-21 07:31:01 -0400

    direction shooting is working!

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface((31, 31))
+        self.image = pygame.Surface((45, 45))
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
 
@@ -55,43 +55,37 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < tileSize * 3:
             self.rect.top = tileSize * 3
 
-    # def getMouse(self):
-    #     self.mousex, self.mousey = pygame.mouse.get_pos()
-
-    def shootBullet(self):
-
-        bullet = Bullets(self.rect.x, self.rect.y)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
+    def shoot(self):
+        x, y = pygame.mouse.get_pos()
+        b = Bullets(RED, player.rect.centerx,
+                    player.rect.centery, 20, 20, 20, x, y)
+        bullets.append(b)
 
 
-class Bullets(pygame.sprite.Sprite):
-    """This defines a bullet class that will shoot out of the sprite"""
+class Bullets(Player):
+    def __init__(self, color, x, y, width, height, speed, targetx, targety):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.speed = 5
+        # get angle to target in radians
+        angle = math.atan2(targety-y, targetx-x)
+        print('Angle in degrees:', int(angle*180/math.pi))
+        self.dx = math.cos(angle)*speed
+        self.dy = math.sin(angle)*speed
+        self.x = x
+        self.y = y
 
-    def __init__(self, dx, dy):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 10))
-        self.image.fill(WHITE)
+    def move(self):
+        # self.x and self.y are floats (decimals) so I get more accuracy
+        # if I change self.x and y and then convert to an integer for
+        # the rectangle.
+        self.x = self.x + self.dx
+        self.y = self.y + self.dy
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
 
-        self.rect = self.image.get_rect()
-        self.rect.bottom = dy
-        self.rect.centerx = dx
-
-    def update(self):
-        self.mouseX, self.mouseY = pygame.mouse.get_pos()
-        self.speed = 8
-        angle = math.atan2(self.mouseY, self.mouseX)
-        print(int(angle*180/math.pi))
-
-        self.dx = math.cos(angle) * self.speed
-        self.dy = math.sin(angle) * self.speed
-
-        self.rect.x = self.rect.x + int(self.dx)
-        self.rect.y = self.rect.y + int(self.dy)
-
-        # kill if it moves off the top of the screen
-        if self.rect.bottom < 0:
-            self.kill()
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
 
 
 def topBar():
@@ -111,7 +105,7 @@ def tileGrid():
 
 
 all_sprites = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
+bullets = []
 
 player = Player()
 
@@ -126,14 +120,16 @@ while running:
             running = False
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
-                player.shootBullet()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
                 player.shoot()
+                # elif event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_SPACE:
+                #         player.shoot()
     all_sprites.update()
 
     gameDisplay.fill(BLACK)
-
+    for b in bullets:
+        b.move()
+        b.draw(gameDisplay)
     # create a grid:
     tileGrid()
     topBar()
